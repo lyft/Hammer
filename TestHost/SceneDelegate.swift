7d911db28bde1b8d93d7969f057264037aea1d20
Kirill Yakimovich <kyakimovich@lyft.com>
2024-06-04 15:34:30 +0200

    Add scenes support to TestHost
    All tests are red now.

---
+++ TestHost/SceneDelegate.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+@available(iOS 13.0, *)
+final class SceneDelegate: UIResponder, UISceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {}
+}
+
